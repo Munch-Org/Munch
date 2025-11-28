ea1db125bb12133bf377935ad3a5ce7b52e6dcd8
midhun kumar <midh0001@student.monash.edu>
2025-11-28 12:54:31 +1100

    feat: add .env file for local dev

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -122,9 +122,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "en-au"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Australia/Melbourne"
 
 USE_I18N = True
 
